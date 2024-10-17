e0e661c28944219071996a358de7e870935fa1cf
dceoy <dceoy@users.noreply.github.com>
2024-10-18 00:44:19 +0900

    Update the ignored Trivy warnings

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -59,6 +59,7 @@ resource "aws_codebuild_webhook" "github" {
   }
 }
 
+# trivy:ignore:avd-aws-0017
 resource "aws_cloudwatch_log_group" "github" {
   name              = "/${var.system_name}/${var.env_type}/codebuild"
   retention_in_days = var.cloudwatch_logs_retention_in_days
