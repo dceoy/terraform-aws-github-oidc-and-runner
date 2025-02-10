3f2f30913109e30ce36de80df4a0a7f1dc008aa7
dceoy <dceoy@users.noreply.github.com>
2025-02-10 22:54:37 +0900

    Update default variables

--- modules/codebuild/variables.tf
+++ modules/codebuild/variables.tf
@@ -1,13 +1,11 @@
 variable "system_name" {
   description = "System name"
   type        = string
-  default     = "gha"
 }
 
 variable "env_type" {
   description = "Environment type"
   type        = string
-  default     = "dev"
 }
 
 variable "github_repositories_requiring_codebuild" {
